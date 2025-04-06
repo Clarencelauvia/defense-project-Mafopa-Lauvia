1e48be537c54c8023289ec3984b1c4f22e9f98bc
ClarenceLauvia <mafopa762@gmail.com>
2025-04-06 01:10:13 +0100

    fix: replace fruitcake/cors with Laravel 10 built-in CORS

--- laravel/backend/app/Http/Kernel.php
+++ laravel/backend/app/Http/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends HttpKernel
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         \Illuminate\Http\Middleware\HandleCors::class,
-        \Fruitcake\Cors\HandleCors::class,
+        // \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class, 
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
@@ -49,7 +49,7 @@ class Kernel extends HttpKernel
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Fruitcake\Cors\HandleCors::class, 
+            // \Fruitcake\Cors\HandleCors::class, 
         ],
     ];
 
@@ -61,7 +61,7 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $middlewareAliases = [
-        'cors' => \Fruitcake\Cors\HandleCors::class,
+        'cors' => \Illuminate\Http\Middleware\HandleCors::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
